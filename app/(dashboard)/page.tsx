c13b7459b50b1f8dad71042d2ffa37498c995b40
VladislavPetruk <vladiislav.petruk@gmail.com>
2024-10-11 20:46:23 +0300

    comments

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable */
 
 import { SyntheticEvent, useState } from 'react';
 import axios from 'axios';
@@ -67,228 +68,227 @@ export default function Home() {
   // }, []);
 
   // if (!data) return null;
+  return null;
 
-  if (isLoading)
-    return (
-      <div className="grid h-full place-content-center">
-        <Loader />
-      </div>
-    );
+  // if (isLoading)
+  //   return (
+  //     <div className="grid h-full place-content-center">
+  //       <Loader />
+  //     </div>
+  //   );
 
-  if (!data) return null;
+  // const TO_DO_TASKS = filterTasks(data, 'to_do');
+  // const IN_PROGRESS_TASKS = filterTasks(data, 'in_progress');
+  // const DONE_TASKS = filterTasks(data, 'done');
 
-  const TO_DO_TASKS = filterTasks(data, 'to_do');
-  const IN_PROGRESS_TASKS = filterTasks(data, 'in_progress');
-  const DONE_TASKS = filterTasks(data, 'done');
+  // console.log(data, isError);
 
-  console.log(data, isError);
+  // const handleSubmit = async (e: SyntheticEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
 
-  const handleSubmit = async (e: SyntheticEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  //   const task = {
+  //     title,
+  //     description,
+  //     // date,
+  //     tag,
+  //     priority,
+  //     status,
+  //   };
 
-    const task = {
-      title,
-      description,
-      // date,
-      tag,
-      priority,
-      status,
-    };
+  //   try {
+  //     const res = await axios.post('/api/tasks', task);
 
-    try {
-      const res = await axios.post('/api/tasks', task);
+  //     console.log(res.data);
 
-      console.log(res.data);
+  //     // if (res.data.error) {
+  //     //   toast.error(res.data.error);
+  //     // }
 
-      // if (res.data.error) {
-      //   toast.error(res.data.error);
-      // }
+  //     // if (!res.data.error) {
+  //     //   toast.success("Task created successfully.");
+  //     //   allTasks();
+  //     //   closeModal();
+  //     // }
+  //   } catch (error) {
+  //     // toast.error("Something went wrong.");
+  //     console.log(error);
+  //   }
+  // };
 
-      // if (!res.data.error) {
-      //   toast.success("Task created successfully.");
-      //   allTasks();
-      //   closeModal();
-      // }
-    } catch (error) {
-      // toast.error("Something went wrong.");
-      console.log(error);
-    }
-  };
+  // const handleGet = async (e: SyntheticEvent<HTMLButtonElement>) => {
+  //   e.preventDefault();
+  //   try {
+  //     const res = await axios.get('/api/tasks');
 
-  const handleGet = async (e: SyntheticEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    try {
-      const res = await axios.get('/api/tasks');
+  //     console.log(res.data);
 
-      console.log(res.data);
+  //     // if (res.data.error) {
+  //     //   toast.error(res.data.error);
+  //     // }
 
-      // if (res.data.error) {
-      //   toast.error(res.data.error);
-      // }
+  //     // if (!res.data.error) {
+  //     //   toast.success("Task created successfully.");
+  //     //   allTasks();
+  //     //   closeModal();
+  //     // }
+  //   } catch (error) {
+  //     // toast.error("Something went wrong.");
+  //     console.log(error);
+  //   }
+  // };
 
-      // if (!res.data.error) {
-      //   toast.success("Task created successfully.");
-      //   allTasks();
-      //   closeModal();
-      // }
-    } catch (error) {
-      // toast.error("Something went wrong.");
-      console.log(error);
-    }
-  };
+  // if (!TO_DO_TASKS || !IN_PROGRESS_TASKS || !DONE_TASKS) return null;
 
-  if (!TO_DO_TASKS || !IN_PROGRESS_TASKS || !DONE_TASKS) return null;
-
-  return (
-    <div className="grid gap-6 lg:grid-cols-3">
-      <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
-        <div className="flex items-center justify-between font-medium">
-          <p className="text-lg">
-            To do {!!TO_DO_TASKS?.length && <span>{TO_DO_TASKS.length}</span>}
-          </p>
-          <div>
-            <Dialog>
-              <DialogTrigger asChild>
-                <Button aria-haspopup="true" size="icon" variant="ghost">
-                  <Plus />
-                  <span className="sr-only">Add a new task</span>
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="sm:max-w-[500px]">
-                <DialogHeader>
-                  <DialogTitle>Add a new task</DialogTitle>
-                </DialogHeader>
-                <div className="grid gap-y-4 py-4">
-                  <div className="grid gap-y-2">
-                    <Label htmlFor="title">Title</Label>
-                    <Input id="title" />
-                  </div>
-                  <div className="grid gap-y-2">
-                    <Label htmlFor="description">Description</Label>
-                    <Input id="description" />
-                  </div>
-                  <div className="grid gap-y-2">
-                    <Label htmlFor="date">Date</Label>
-                    <Popover>
-                      <PopoverTrigger asChild>
-                        <Button
-                          variant={'outline'}
-                          className={cn(
-                            'justify-start text-left font-normal',
-                            !date && 'text-muted-foreground'
-                          )}
-                        >
-                          <CalendarIcon className="mr-2 h-4 w-4" />
-                          {date ? (
-                            format(date, 'PPP')
-                          ) : (
-                            <span>Select a date</span>
-                          )}
-                        </Button>
-                      </PopoverTrigger>
-                      <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar
-                          mode="single"
-                          selected={date}
-                          onSelect={setDate}
-                          initialFocus
-                        />
-                      </PopoverContent>
-                    </Popover>
-                  </div>
-                  <div className="grid gap-y-2">
-                    <Label htmlFor="priority">Priority</Label>
-                    <Select>
-                      <SelectTrigger>
-                        <SelectValue placeholder="Select a task priority" />
-                      </SelectTrigger>
-                      <SelectContent>
-                        <SelectItem value="low" className="cursor-pointer">
-                          <div className="flex items-center gap-3">
-                            <div
-                              className={cn(`h-4 w-4 rounded-full`)}
-                              style={{
-                                backgroundColor: PRIORITY_COLORS['low'],
-                              }}
-                            ></div>
-                            Low
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="medium" className="cursor-pointer">
-                          <div className="flex items-center gap-3">
-                            <div
-                              className={cn(`h-4 w-4 rounded-full`)}
-                              style={{
-                                backgroundColor: PRIORITY_COLORS['medium'],
-                              }}
-                            ></div>
-                            Medium
-                          </div>
-                        </SelectItem>
-                        <SelectItem value="high" className="cursor-pointer">
-                          <div className="flex items-center gap-3">
-                            <div
-                              className={cn(`h-4 w-4 rounded-full`)}
-                              style={{
-                                backgroundColor: PRIORITY_COLORS['high'],
-                              }}
-                            ></div>
-                            High
-                          </div>
-                        </SelectItem>
-                      </SelectContent>
-                    </Select>
-                  </div>
-                </div>
-                <DialogFooter>
-                  <Button type="submit" className="pl-3" onClick={handleSubmit}>
-                    <Plus className="mr-1" size={20} /> Create a task
-                  </Button>
-                  <Button type="submit" className="pl-3" onClick={handleGet}>
-                    <Plus className="mr-1" size={20} /> Get tasks
-                  </Button>
-                </DialogFooter>
-              </DialogContent>
-            </Dialog>
-            {/* <Button aria-haspopup="true" size="icon" variant="ghost">
-            <EllipsisVertical />
-              <span className="sr-only">Toggle card handler</span>
-            </Button> */}
-          </div>
-        </div>
-        {TO_DO_TASKS?.map((task) => (
-          // <div data-swapy-slot={task.id} key={task.id}>
-          <TaskCard task={task} key={task.id} />
-          // </div>
-        ))}
-      </div>
-      <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
-        <div className="flex items-center justify-between font-medium">
-          <p className="text-lg">
-            In progress{' '}
-            {!!IN_PROGRESS_TASKS?.length && (
-              <span>{IN_PROGRESS_TASKS.length}</span>
-            )}
-          </p>
-        </div>
-        {IN_PROGRESS_TASKS?.map((task) => (
-          // <div data-swapy-slot={task.id} key={task.id}>
-          <TaskCard task={task} key={task.id} />
-          // </div>
-        ))}
-      </div>
-      <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
-        <div className="flex items-center justify-between font-medium">
-          <p className="text-lg">
-            Done {!!DONE_TASKS?.length && <span>{DONE_TASKS.length}</span>}
-          </p>
-        </div>
-        {DONE_TASKS?.map((task) => (
-          // <div data-swapy-slot={task.id} key={task.id}>
-          <TaskCard task={task} key={task.id} />
-          // </div>
-        ))}
-      </div>
-    </div>
-  );
+  // return (
+  //   <div className="grid gap-6 lg:grid-cols-3">
+  //     <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
+  //       <div className="flex items-center justify-between font-medium">
+  //         <p className="text-lg">
+  //           To do {!!TO_DO_TASKS?.length && <span>{TO_DO_TASKS.length}</span>}
+  //         </p>
+  //         <div>
+  //           <Dialog>
+  //             <DialogTrigger asChild>
+  //               <Button aria-haspopup="true" size="icon" variant="ghost">
+  //                 <Plus />
+  //                 <span className="sr-only">Add a new task</span>
+  //               </Button>
+  //             </DialogTrigger>
+  //             <DialogContent className="sm:max-w-[500px]">
+  //               <DialogHeader>
+  //                 <DialogTitle>Add a new task</DialogTitle>
+  //               </DialogHeader>
+  //               <div className="grid gap-y-4 py-4">
+  //                 <div className="grid gap-y-2">
+  //                   <Label htmlFor="title">Title</Label>
+  //                   <Input id="title" />
+  //                 </div>
+  //                 <div className="grid gap-y-2">
+  //                   <Label htmlFor="description">Description</Label>
+  //                   <Input id="description" />
+  //                 </div>
+  //                 <div className="grid gap-y-2">
+  //                   <Label htmlFor="date">Date</Label>
+  //                   <Popover>
+  //                     <PopoverTrigger asChild>
+  //                       <Button
+  //                         variant={'outline'}
+  //                         className={cn(
+  //                           'justify-start text-left font-normal',
+  //                           !date && 'text-muted-foreground'
+  //                         )}
+  //                       >
+  //                         <CalendarIcon className="mr-2 h-4 w-4" />
+  //                         {date ? (
+  //                           format(date, 'PPP')
+  //                         ) : (
+  //                           <span>Select a date</span>
+  //                         )}
+  //                       </Button>
+  //                     </PopoverTrigger>
+  //                     <PopoverContent className="w-auto p-0" align="start">
+  //                       <Calendar
+  //                         mode="single"
+  //                         selected={date}
+  //                         onSelect={setDate}
+  //                         initialFocus
+  //                       />
+  //                     </PopoverContent>
+  //                   </Popover>
+  //                 </div>
+  //                 <div className="grid gap-y-2">
+  //                   <Label htmlFor="priority">Priority</Label>
+  //                   <Select>
+  //                     <SelectTrigger>
+  //                       <SelectValue placeholder="Select a task priority" />
+  //                     </SelectTrigger>
+  //                     <SelectContent>
+  //                       <SelectItem value="low" className="cursor-pointer">
+  //                         <div className="flex items-center gap-3">
+  //                           <div
+  //                             className={cn(`h-4 w-4 rounded-full`)}
+  //                             style={{
+  //                               backgroundColor: PRIORITY_COLORS['low'],
+  //                             }}
+  //                           ></div>
+  //                           Low
+  //                         </div>
+  //                       </SelectItem>
+  //                       <SelectItem value="medium" className="cursor-pointer">
+  //                         <div className="flex items-center gap-3">
+  //                           <div
+  //                             className={cn(`h-4 w-4 rounded-full`)}
+  //                             style={{
+  //                               backgroundColor: PRIORITY_COLORS['medium'],
+  //                             }}
+  //                           ></div>
+  //                           Medium
+  //                         </div>
+  //                       </SelectItem>
+  //                       <SelectItem value="high" className="cursor-pointer">
+  //                         <div className="flex items-center gap-3">
+  //                           <div
+  //                             className={cn(`h-4 w-4 rounded-full`)}
+  //                             style={{
+  //                               backgroundColor: PRIORITY_COLORS['high'],
+  //                             }}
+  //                           ></div>
+  //                           High
+  //                         </div>
+  //                       </SelectItem>
+  //                     </SelectContent>
+  //                   </Select>
+  //                 </div>
+  //               </div>
+  //               <DialogFooter>
+  //                 <Button type="submit" className="pl-3" onClick={handleSubmit}>
+  //                   <Plus className="mr-1" size={20} /> Create a task
+  //                 </Button>
+  //                 <Button type="submit" className="pl-3" onClick={handleGet}>
+  //                   <Plus className="mr-1" size={20} /> Get tasks
+  //                 </Button>
+  //               </DialogFooter>
+  //             </DialogContent>
+  //           </Dialog>
+  //           {/* <Button aria-haspopup="true" size="icon" variant="ghost">
+  //           <EllipsisVertical />
+  //             <span className="sr-only">Toggle card handler</span>
+  //           </Button> */}
+  //         </div>
+  //       </div>
+  //       {TO_DO_TASKS?.map((task) => (
+  //         // <div data-swapy-slot={task.id} key={task.id}>
+  //         <TaskCard task={task} key={task.id} />
+  //         // </div>
+  //       ))}
+  //     </div>
+  //     <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
+  //       <div className="flex items-center justify-between font-medium">
+  //         <p className="text-lg">
+  //           In progress{' '}
+  //           {!!IN_PROGRESS_TASKS?.length && (
+  //             <span>{IN_PROGRESS_TASKS.length}</span>
+  //           )}
+  //         </p>
+  //       </div>
+  //       {IN_PROGRESS_TASKS?.map((task) => (
+  //         // <div data-swapy-slot={task.id} key={task.id}>
+  //         <TaskCard task={task} key={task.id} />
+  //         // </div>
+  //       ))}
+  //     </div>
+  //     <div className="grid grid-rows-[max-content] gap-y-4 rounded-xl bg-gray-200 p-6">
+  //       <div className="flex items-center justify-between font-medium">
+  //         <p className="text-lg">
+  //           Done {!!DONE_TASKS?.length && <span>{DONE_TASKS.length}</span>}
+  //         </p>
+  //       </div>
+  //       {DONE_TASKS?.map((task) => (
+  //         // <div data-swapy-slot={task.id} key={task.id}>
+  //         <TaskCard task={task} key={task.id} />
+  //         // </div>
+  //       ))}
+  //     </div>
+  //   </div>
+  // );
 }
